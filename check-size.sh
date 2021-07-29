2e6ba9ff3b10a6365b4061bc9927aa03bf4ec6b6
Rahul Sangole <rsangole@u.northwestern.edu>
2021-07-29 07:21:31 -0700

    updates readme

--- check-size.sh
+++ check-size.sh
@@ -2,4 +2,4 @@
 
 docker exec postgres_db_1 bash -c "psql -U rahul -d anomaly -c '\d+'"
 
-# docker exec postgres_db_1 bash -c "psql -U rahul -d anomaly -c 'SELECT pg_size_pretty(pg_total_relation_size(\'anomaly\'))'"
+docker exec postgres_db_1 bash -c "psql -U rahul -d anomaly -c 'SELECT pg_database.datname, pg_size_pretty(pg_database_size(pg_database.datname)) AS size FROM pg_database;'"
